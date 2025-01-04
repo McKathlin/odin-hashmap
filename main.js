60e8a2636b1febe6f0bc8a98a99af570367aaf17
McKathlin <mckathlin@gmail.com>
2025-01-04 20:17:12 +0000

    Add main.js as basic test for HashMap

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
 import HashMap from "./hashmap.js";
 
-let hmap = new HashMap();
-console.log(hmap);
+let test = new HashMap();
+
+// Populate
+test.set('apple', 'red');
+test.set('banana', 'yellow');
+test.set('carrot', 'orange');
+test.set('dog', 'brown');
+test.set('elephant', 'gray');
+test.set('frog', 'green');
+test.set('grape', 'purple');
+test.set('hat', 'black');
+test.set('ice cream', 'white');
+test.set('jacket', 'blue');
+test.set('kite', 'pink');
+test.set('lion', 'golden');
+
+// Change values of existing keys
+test.set('apple', 'green');
+test.set('jacket', 'purple');
+test.set('hat', 'white');
+
+// One more key
+test.set('moon', 'silver');
+
+console.log(test);
